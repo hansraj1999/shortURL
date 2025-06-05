3fb5e0bf8ae1a0d2236d36bf930f91646c4017b1
unknown <deghun@gmail.com>
2025-06-05 17:10:19 +0530

    changed port no

--- entrypoint.py
+++ entrypoint.py
@@ -3,5 +3,5 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:start_server", port=8000, host="0.0.0.0", reload=True, factory=True
+        "main:start_server", port=8080, host="0.0.0.0", reload=True, factory=True
     )
