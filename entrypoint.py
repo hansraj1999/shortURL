54c7d97054c59bb3c609cafda296cbfa3f72eca8
unknown <deghun@gmail.com>
2025-06-05 20:07:50 +0530

    bug fixed

--- entrypoint.py
+++ entrypoint.py
@@ -2,9 +2,16 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "main:start_server",
-        port=int(os.environ.get("PORT", 8000)),
-        host="0.0.0.0",
-        factory=True,
-    )
+    import traceback
+
+    try:
+        uvicorn.run(
+            "main:start_server",
+            port=8000,
+            host="0.0.0.0",
+            factory=True,
+        )
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        os._exit(1)
