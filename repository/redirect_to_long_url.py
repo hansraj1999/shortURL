632b5d25ce292c46c0945207b391724ad7b1e238
unknown <deghun@gmail.com>
2025-06-07 14:01:40 +0530

    raised exception

--- repository/redirect_to_long_url.py
+++ repository/redirect_to_long_url.py
@@ -11,15 +11,22 @@ class Handler:
         self.url_hash = url_hash
 
     def handle(self) -> str:
-        actual_url = config.backend.get_url_from_cache(self.url_hash)
-        if actual_url:
-            logger.info("found in cache")
-            return actual_url
-        else:
-            logger.info("not found on cache fetching from disk")
-            doc = config.backend.fetch_long_url(self.url_hash)
-            if not doc:
-                raise HTTPException(
-                    status_code=404, detail={"message": "URL not found", "details": []}
-                )
-            return doc["actual_url"]
+        try:
+            actual_url = config.backend.get_url_from_cache(self.url_hash)
+            if actual_url:
+                logger.info("found in cache")
+                return actual_url
+            else:
+                logger.info("not found on cache fetching from disk")
+                doc = config.backend.fetch_long_url(self.url_hash)
+                if not doc:
+                    raise HTTPException(
+                        status_code=404,
+                        detail={"message": "URL not found", "details": []},
+                    )
+                return doc["actual_url"]
+        except Exception as e:
+            logger.exception(e)
+            raise HTTPException(
+                status_code=500, detail={"message": str(e), "details": []}
+            )
